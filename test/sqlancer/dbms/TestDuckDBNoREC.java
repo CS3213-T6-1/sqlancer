5028464618e062471b447cdd5107dcb121c3cc84
liauzhanyi <liauzhanyi@gmail.com>
2025-03-27 16:36:14 +0800

    Add custom table test for DuckDB in TestDuckDBNoREC

--- test/sqlancer/dbms/TestDuckDBNoREC.java
+++ test/sqlancer/dbms/TestDuckDBNoREC.java
@@ -9,8 +9,18 @@ import sqlancer.Main;
 public class TestDuckDBNoREC {
     @Test
     public void testDuckDBNoREC() {
-        // run with one thread due to multithreading issues, see https://github.com/sqlancer/sqlancer/pull/45
+        // run with one thread due to multithreading issues, see
+        // https://github.com/sqlancer/sqlancer/pull/45
         assertEquals(0, Main.executeMain(new String[] { "--random-seed", "0", "--timeout-seconds", TestConfig.SECONDS,
                 "--num-threads", "1", "--num-queries", TestConfig.NUM_QUERIES, "duckdb", "--oracle", "NOREC" }));
+    }
+
+    @Test
+    public void testDuckDBNoREC_customTable() {
+        // run with one thread due to multithreading issues, see
+        // https://github.com/sqlancer/sqlancer/pull/45
+        assertEquals(0, Main.executeMain(new String[] { "--random-seed", "0", "--timeout-seconds", TestConfig.SECONDS,
+                "--num-threads", "1", "--num-queries", TestConfig.NUM_QUERIES, "duckdb", "--oracle", "NOREC",
+                "--use-custom-script", "test/sqlancer/custom_tables.sql" }));
     }
 }
