fe2669a030412508e1c00721f046c9dcd6374a74
yewjon <e0726230@u.nus.edu>
2025-04-09 14:30:04 +0800

    reformat

--- test/sqlancer/citus/gen/CitusUpdateGenerator_ESTest.java
+++ test/sqlancer/citus/gen/CitusUpdateGenerator_ESTest.java
@@ -9,35 +9,35 @@ import sqlancer.postgres.PostgresGlobalState;
 //@RunWith(EvoRunner.class) @EvoRunnerParameters(mockJVMNonDeterminism = true, useVFS = true, useVNET = true, resetStaticState = true, separateClassLoader = true)
 public class CitusUpdateGenerator_ESTest {
 
-  @Test
-  public void test0()  throws Throwable  {
-      PostgresGlobalState postgresGlobalState0 = new PostgresGlobalState();
-      // Undeclared exception!
-      try { 
-        CitusUpdateGenerator.create(postgresGlobalState0);
-        fail("Expecting exception: AssertionError");
-      
-      } catch(AssertionError e) {
-         //
-         // null
-         //
-      }
-  }
+    @Test
+    public void test0() throws Throwable {
+        PostgresGlobalState postgresGlobalState0 = new PostgresGlobalState();
+        // Undeclared exception!
+        try {
+            CitusUpdateGenerator.create(postgresGlobalState0);
+            fail("Expecting exception: AssertionError");
 
-  @Test
-  public void test1()  throws Throwable  {
-      // Undeclared exception!
-      try { 
-        CitusUpdateGenerator.create((PostgresGlobalState) null);
-        fail("Expecting exception: NullPointerException");
-      
-      } catch(NullPointerException e) {
-         //
-         // no message in exception (getMessage() returned null)
-         //
-         verifyException("sqlancer.postgres.gen.PostgresUpdateGenerator", e);
-      }
-  }
+        } catch (AssertionError e) {
+            //
+            // null
+            //
+        }
+    }
+
+    @Test
+    public void test1() throws Throwable {
+        // Undeclared exception!
+        try {
+            CitusUpdateGenerator.create((PostgresGlobalState) null);
+            fail("Expecting exception: NullPointerException");
+
+        } catch (NullPointerException e) {
+            //
+            // no message in exception (getMessage() returned null)
+            //
+            verifyException("sqlancer.postgres.gen.PostgresUpdateGenerator", e);
+        }
+    }
 
     private void verifyException(String s, NullPointerException e) {
     }
